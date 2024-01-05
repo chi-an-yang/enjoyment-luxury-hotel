671310fedbf598da6a15c43ba15d6e880dbfc225
BolasLien <bolaslien@gmail.com>
2024-01-06 02:11:00 +0800

    feat: 客製化 Mui 樣式

--- src/pages/ExamplePage/components/MuiComponents.tsx
+++ src/pages/ExamplePage/components/MuiComponents.tsx
@@ -1,4 +1,6 @@
-import { Stack, Typography, Button, TextField } from '@mui/material';
+import { Stack, Typography, Button, TextField, Link } from '@mui/material';
+import theme from '@src/theme';
+import InputField from '@src/ui-components/InputField';
 
 const MuiComponents = () => {
   return (
@@ -6,14 +8,28 @@ const MuiComponents = () => {
       <Stack spacing={2}>
         <Typography variant="subtitle1">Button</Typography>
         <Stack spacing={2} direction="row">
-          <Button variant="contained">Contained</Button>
-          <Button variant="outlined">Outlined</Button>
+          <Button variant="contained">會員登入</Button>
+          <Button disabled variant="contained">
+            會員登入
+          </Button>
         </Stack>
       </Stack>
       <Stack spacing={2}>
         <Typography variant="subtitle1">Text Field</Typography>
+
         <Stack spacing={2} direction="row">
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          <TextField variant="outlined" placeholder="請輸入密碼" value="jessica@sample.com" />
+          <TextField variant="outlined" placeholder="請輸入密碼" />
+        </Stack>
+        <Stack spacing={2} bgcolor={(theme)=> theme.palette.neutral[40]} p={2}>
+          <Typography variant="subtitle1">本次需求元件</Typography>
+          <InputField labelStart="密碼" labelEnd="必填" defaultValue="" placeholder="請輸入密碼" />
+        </Stack>
+      </Stack>
+      <Stack spacing={2}>
+        <Typography variant="subtitle1">Link</Typography>
+        <Stack spacing={2} direction="row">
+          <Link href="https://google.com">忘記密碼？</Link>
         </Stack>
       </Stack>
     </Stack>
