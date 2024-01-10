af69302600da413aa34155be8faea21210f2f679
BolasLien <bolaslien@gmail.com>
2024-01-10 14:07:18 +0800

    chore: add css reset

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@emotion/react';
 import theme from './theme';
-
+import { CssBaseline } from '@mui/material';
 
 function App() {
   const queryClient = new QueryClient();
@@ -11,6 +11,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
+        <CssBaseline />
         <BrowserRouter>
           <Router />
         </BrowserRouter>
