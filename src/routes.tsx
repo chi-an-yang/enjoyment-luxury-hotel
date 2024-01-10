069b8bb70e37107d7905bd8fb914aafd8b7b31e1
BolasLien <bolaslien@gmail.com>
2024-01-10 14:20:56 +0800

    chore: add pages/common files and demo code

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,15 @@
-import {  Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 // pages
 import ExamplePage from './pages/ExamplePage';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" index element={<ExamplePage />} />
+      <Route path="/home" index element={<HomePage />} />
+      <Route path="/login" index element={<LoginPage />} />
     </Routes>
   );
 }
