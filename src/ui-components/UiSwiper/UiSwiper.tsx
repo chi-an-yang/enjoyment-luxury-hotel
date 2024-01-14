fb044d95db5eb3dd77f38b1a6c28133d8ce22505
Chian <chian0322@gmail.com>
2024-01-14 17:47:28 +0800

    feat: add UiSwiper, ExampleSwiperPage, theme hotelPrimary

---
+++ src/ui-components/UiSwiper/UiSwiper.tsx
@@ -0,0 +1,111 @@
+import { Box, useTheme } from '@mui/material';
+// import required modules
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation, Autoplay } from 'swiper/modules';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+interface IItems {
+  src: string;
+  name: string;
+}
+
+interface IUiSwiper {
+  items: IItems[];
+  navigation?: boolean;
+}
+
+const UiSwiper: React.FC<IUiSwiper> = ({ items, navigation = false }) => {
+  const { palette } = useTheme();
+  const UiSwiperStyle = {
+    '.swiper': {
+      width: '100%',
+      height: '100%',
+    },
+    '.swiper-pagination-bullet': {
+      width: '32px',
+      height: '4px',
+      borderRadius: '100px',
+      opacity: 1,
+      backgroundColor: palette.hotelPrimary[40],
+    },
+    '.swiper-pagination-bullet-active': {
+      width: '60px',
+      height: '4px',
+      borderRadius: '100px',
+      backgroundColor: palette.hotelPrimary[100],
+    },
+    '.swiper-slide': {
+      background: '#fff',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    '.swiper-slide img': {
+      display: 'block',
+      width: '100%',
+      height: '100%',
+      objectFit: 'cover',
+    },
+    '.swiper-button-prev': {
+      width: '56px',
+      height: '56px',
+      padding: '8px',
+      borderRadius: '100px',
+      backgroundColor: '#FFF',
+      '&:after': {
+        display: 'none',
+      },
+      '&:before': {
+        content: '""',
+        width: '40px',
+        height: '40px',
+        backgroundImage: `url('/src/assets/icons/arrow_left.svg')`,
+      },
+    },
+    '.swiper-button-next': {
+      width: '56px',
+      height: '56px',
+      padding: '8px',
+      borderRadius: '100px',
+      backgroundColor: '#FFF',
+      '&:after': {
+        display: 'none',
+      },
+      '&:before': {
+        content: '""',
+        width: '40px',
+        height: '40px',
+        backgroundImage: `url('/src/assets/icons/arrow_right.svg')`,
+      },
+    },
+  };
+  return (
+    <Box width={'100%'} height={'100%'} sx={UiSwiperStyle}>
+      <Swiper
+        slidesPerView={1}
+        loop={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={navigation}
+        modules={[Pagination, Navigation, Autoplay]}
+        className="mySwiper"
+      >
+        {items.map(({ src, name }) => (
+          <SwiperSlide key={name}>
+            <img src={src} alt={name} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Box>
+  );
+};
+
+export default UiSwiper;
