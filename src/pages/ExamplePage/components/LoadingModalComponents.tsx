7edc1e26be0dc59a15131723e9cbed52e5a3934e
emi.chang <emi.chang@aoba.tw>
2024-02-03 21:40:46 +0800

    feat(component): refactor LoadingModal-2

--- src/pages/ExamplePage/components/LoadingModalComponents.tsx
+++ src/pages/ExamplePage/components/LoadingModalComponents.tsx
@@ -1,69 +1,37 @@
-import { Stack, Typography, Button, CircularProgress, useTheme } from '@mui/material';
+import { Button, Stack } from '@mui/material';
 import { useState } from 'react';
-import LoadingModal from '../../../ui-components/LoadingModal';
-import Logo from '@src/assets/logo.svg?react';
+import LoadingModal from '@src/ui-components/LoadingModal';
 import axios, { AxiosError } from 'axios';
-import { MdClose } from 'react-icons/md';
-import { VariantType, useSnackbar, SnackbarKey, SnackbarOrigin } from 'notistack';
+import { useSnackbar } from 'notistack';
 
-type ResultType = 'success' | 'fail';
 const LoadingModalComponents = () => {
-  const { palette } = useTheme();
   const [isDialogOpen, setDialogOpen] = useState(false);
   const openDialog = () => setDialogOpen(true);
   const closeDialog = () => setDialogOpen(false);
-
+  const { enqueueSnackbar } = useSnackbar();
   const testUrls = {
     success: 'https://jsonplaceholder.typicode.com/todos/1',
     fail: 'https://...',
   };
-  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
-  const action = (snackbarId: SnackbarKey) => {
-    return (
-      <MdClose
-        onClick={() => {
-          closeSnackbar(snackbarId);
-        }}
-      />
-    );
-  };
 
-  const handleShowToast = ({
-    message = 'Update success',
-    anchorOrigin = { horizontal: 'left', vertical: 'bottom' },
-    variant = 'success',
-  }: {
-    message: string;
-    anchorOrigin?: SnackbarOrigin;
-    variant: VariantType;
-  }) => {
-    enqueueSnackbar(message, { variant, anchorOrigin, action });
-  };
-
-  const fetchData = async (type: ResultType) =>
+  const fetchData = async (url: string) =>
     axios
-      .get(testUrls[type])
+      .get(url)
       .then((res) => {
         if (res) console.log(res);
-        handleShowToast({
-          message: '預約成功，請至個人頁面查看詳情',
-          variant: 'success',
-        });
+        enqueueSnackbar('預約成功，請至個人頁面查看詳情', { variant: 'success' });
       })
       .catch((err: AxiosError) => {
-        handleShowToast({
-          message: '訂單傳送失敗，請稍後再試',
-          variant: 'error',
-        });
+        enqueueSnackbar('訂單傳送失敗，請稍後再試', { variant: 'error' });
         throw err;
       });
 
-  const handleClick = (type: ResultType): void => {
+  const handleClick = (type: 'success' | 'fail'): void => {
     openDialog();
 
     // fetch 時間太短了所以加個 setTimeout
     const timerId = setTimeout(() => {
-      fetchData(type);
+      fetchData(testUrls[type]);
       closeDialog();
       clearTimeout(timerId);
     }, 2000);
@@ -77,17 +45,7 @@ const LoadingModalComponents = () => {
       <Button variant="contained" onClick={() => handleClick('fail')}>
         讀取彈窗（失敗）
       </Button>
-      <LoadingModal open={isDialogOpen}>
-        <Stack flex="row" alignItems="center" py={12} px={20}>
-          <Stack flex="row" alignItems="center">
-            <CircularProgress />
-          </Stack>
-          <Stack flex="row" alignItems="center" width={100}>
-            <Logo width="100%" fill={palette.primary.main} />
-          </Stack>
-          <Typography fontWeight="bold">正在處理你的預定</Typography>
-        </Stack>
-      </LoadingModal>
+      <LoadingModal open={isDialogOpen} />
     </Stack>
   );
 };
--- src/ui-components/LoadingModal/LoadingModal.tsx
+++ src/ui-components/LoadingModal/LoadingModal.tsx
@@ -1,18 +1,37 @@
-import { Dialog, DialogContent } from '@mui/material';
+import {
+  CircularProgress,
+  Dialog,
+  DialogContent,
+  Stack,
+  Typography,
+  useTheme,
+} from '@mui/material';
+import Logo from '@src/assets/logo.svg?react';
 import { ReactNode } from 'react';
 
 interface LoadingModalProps {
   open: boolean;
   children?: ReactNode;
 }
-const LoadingModal = ({ open, children }: LoadingModalProps) => {
+const LoadingModal = ({ open }: LoadingModalProps) => {
+  const { palette } = useTheme();
   return (
     <Dialog
       open={open}
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
     >
-      <DialogContent>{children}</DialogContent>
+      <DialogContent>
+        <Stack flex="row" alignItems="center" py={12} px={20}>
+          <Stack flex="row" alignItems="center">
+            <CircularProgress />
+          </Stack>
+          <Stack flex="row" alignItems="center" width={100}>
+            <Logo width="100%" fill={palette.primary.main} />
+          </Stack>
+          <Typography fontWeight="bold">正在處理你的預定</Typography>
+        </Stack>
+      </DialogContent>
     </Dialog>
   );
 };
