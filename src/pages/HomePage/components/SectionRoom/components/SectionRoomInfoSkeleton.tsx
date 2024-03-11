9ac54f4ad8615d19762048c7a5e9921f242c8159
Chian <chian0322@gmail.com>
2024-03-11 22:55:44 +0800

    refactor(sectionroominfoskeleton): useMediaQuery to adjust style

--- src/pages/HomePage/components/SectionRoom/components/SectionRoomInfoSkeleton.tsx
+++ src/pages/HomePage/components/SectionRoom/components/SectionRoomInfoSkeleton.tsx
@@ -1,107 +1,54 @@
 import { useTheme, Skeleton, useMediaQuery } from '@mui/material';
 
-const SectionRoomInfoSkeletonDesktop = () => {
-  const { palette } = useTheme();
-  return (
-    <>
-      <Skeleton
-        variant="text"
-        sx={{
-          width: '100%',
-          fontSize: '40px',
-          color: palette.neutral[0],
-          bgcolor: 'grey.900',
-          mb: 2,
-        }}
-      />
-      <Skeleton
-        variant="text"
-        sx={{
-          width: '100%',
-          fontSize: '16px',
-          color: palette.neutral[0],
-          bgcolor: 'grey.900',
-          mb: 5,
-        }}
-      />
-      <Skeleton
-        variant="text"
-        sx={{
-          width: '100%',
-          fontSize: '32px',
-          color: palette.neutral[0],
-          bgcolor: 'grey.900',
-          mb: 5,
-        }}
-      />
-      <Skeleton
-        variant="rounded"
-        sx={{
-          width: '100%',
-          maxWidth: '628px',
-          height: '116px',
-          borderRadius: '8px',
-          bgcolor: 'grey.900',
-          mb: 5,
-        }}
-      />
-    </>
-  );
-};
-
-const SectionRoomInfoSkeletonMobile = () => {
-  const { palette } = useTheme();
-  return (
-    <>
-      <Skeleton
-        variant="text"
-        sx={{
-          width: '100%',
-          fontSize: '28px',
-          color: palette.neutral[0],
-          bgcolor: 'grey.900',
-          mb: 2,
-        }}
-      />
-      <Skeleton
-        variant="text"
-        sx={{
-          width: '100%',
-          fontSize: '14px',
-          color: palette.neutral[0],
-          bgcolor: 'grey.900',
-          mb: 3,
-        }}
-      />
-      <Skeleton
-        variant="text"
-        sx={{
-          width: '100%',
-          fontSize: '16px',
-          color: palette.neutral[0],
-          bgcolor: 'grey.900',
-          mb: 3,
-        }}
-      />
-      <Skeleton
-        variant="rounded"
-        sx={{
-          width: '100%',
-          maxWidth: '351px',
-          height: '64px',
-          borderRadius: '8px',
-          bgcolor: 'grey.900',
-          mb: 3,
-        }}
-      />
-    </>
-  );
-};
-
 const SectionRoomInfoSkeleton = () => {
+  const { palette } = useTheme();
   const { breakpoints } = useTheme();
   const isDesktop = useMediaQuery(breakpoints.up('md'));
-  return isDesktop ? <SectionRoomInfoSkeletonDesktop /> : <SectionRoomInfoSkeletonMobile />;
+  return (
+    <>
+      <Skeleton
+        variant="text"
+        sx={{
+          width: '100%',
+          fontSize: isDesktop ? '40px' : '28px',
+          color: palette.neutral[0],
+          bgcolor: 'grey.900',
+          mb: 2,
+        }}
+      />
+      <Skeleton
+        variant="text"
+        sx={{
+          width: '100%',
+          fontSize: isDesktop ? '16px' : '14px',
+          color: palette.neutral[0],
+          bgcolor: 'grey.900',
+          mb: isDesktop ? 5 : 3,
+        }}
+      />
+      <Skeleton
+        variant="text"
+        sx={{
+          width: '100%',
+          fontSize: isDesktop ? '32px' : '16px',
+          color: palette.neutral[0],
+          bgcolor: 'grey.900',
+          mb: isDesktop ? 5 : 3,
+        }}
+      />
+      <Skeleton
+        variant="rounded"
+        sx={{
+          width: '100%',
+          maxWidth: isDesktop ? '628px' : '351px',
+          height: isDesktop ? '116px' : '64px',
+          borderRadius: '8px',
+          bgcolor: 'grey.900',
+          mb: isDesktop ? 5 : 3,
+        }}
+      />
+    </>
+  );
 };
 
 export default SectionRoomInfoSkeleton;
