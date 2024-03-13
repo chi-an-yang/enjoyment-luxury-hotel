84c2bf4ca3b331dde5dd3480ba864a591eecb3ac
Chian <chian0322@gmail.com>
2024-03-13 21:32:59 +0800

    fix(decotop&decobottom): use useMediaQuery to adjust style

--- src/pages/HomePage/components/SectionNews/components/DecoBottom.tsx
+++ src/pages/HomePage/components/SectionNews/components/DecoBottom.tsx
@@ -1,26 +1,20 @@
 import { Box, useTheme, useMediaQuery } from '@mui/material';
 import Dot from '@src/assets/images/dot.png';
 
-const DecoBottomDesktop = () => {
-  return (
-    <Box position={'absolute'} bottom={'-80px'} left={'200px'}>
-      <img src={Dot} alt="deco" width="100%" height="100%" />
-    </Box>
-  );
-};
-
-const DecoBottomMobile = () => {
-  return (
-    <Box position={'absolute'} bottom={'-60px'} left={'24px'} width={'100px'} height={'100px'}>
-      <img src={Dot} alt="deco" width="100%" height="100%" />
-    </Box>
-  );
-};
-
 const DecoBottom = () => {
   const { breakpoints } = useTheme();
   const isDesktop = useMediaQuery(breakpoints.up('md'));
-  return isDesktop ? <DecoBottomDesktop /> : <DecoBottomMobile />;
+  return (
+    <Box
+      position={'absolute'}
+      bottom={isDesktop ? '-80px' : '-60px'}
+      left={isDesktop ? '200px' : '24px'}
+      width={isDesktop ? '200px' : '100px'}
+      height={isDesktop ? '200px' : '100px'}
+    >
+      <img src={Dot} alt="deco" width="100%" height="100%" />
+    </Box>
+  );
 };
 
 export default DecoBottom;
--- src/pages/HomePage/components/SectionNews/components/DecoTop.tsx
+++ src/pages/HomePage/components/SectionNews/components/DecoTop.tsx
@@ -1,26 +1,20 @@
 import { Box, useTheme, useMediaQuery } from '@mui/material';
 import Dot from '@src/assets/images/dot.png';
 
-const DecoTopDesktop = () => {
-  return (
-    <Box position={'absolute'} top={'100px'} right={'180px'}>
-      <img src={Dot} alt="deco" width="100%" height="100%" />
-    </Box>
-  );
-};
-
-const DecoTopMobile = () => {
-  return (
-    <Box position={'absolute'} top={'40px'} right={'24px'} width={'100px'} height={'100px'}>
-      <img src={Dot} alt="deco" width="100%" height="100%" />
-    </Box>
-  );
-};
-
 const DecoTop = () => {
   const { breakpoints } = useTheme();
   const isDesktop = useMediaQuery(breakpoints.up('md'));
-  return isDesktop ? <DecoTopDesktop /> : <DecoTopMobile />;
+  return (
+    <Box
+      position={'absolute'}
+      top={isDesktop ? '100px' : '40px'}
+      right={isDesktop ? '180px' : '24px'}
+      width={isDesktop ? '200px' : '100px'}
+      height={isDesktop ? '200px' : '100px'}
+    >
+      <img src={Dot} alt="deco" width="100%" height="100%" />
+    </Box>
+  );
 };
 
 export default DecoTop;
