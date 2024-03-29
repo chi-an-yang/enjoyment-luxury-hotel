cb1d8418d16011599e740399994a8c8b8c2a05fb
Chian <chian0322@gmail.com>
2024-03-29 22:17:56 +0800

    refactor(roominfobasicimage): switch =>  hashMap

--- src/pages/RoomPage/components/RoomInfoBasicImage.tsx
+++ src/pages/RoomPage/components/RoomInfoBasicImage.tsx
@@ -4,17 +4,17 @@ import maxPeopleImg from '@src/assets/images/maxPeople.png';
 import isProvideImg from '@src/assets/images/isProvide.png';
 
 type imageProps = 'areaInfo' | 'bedInfo' | 'maxPeople' | 'isProvide';
+
+const imageMap = {
+  areaInfo: areaInfoImg,
+  bedInfo: bedInfoImg,
+  maxPeople: maxPeopleImg,
+  isProvide: isProvideImg,
+};
+
 const RoomInfoBasicImage = ({ type }: { type: imageProps }) => {
-  switch (type) {
-    case 'areaInfo':
-      return <img src={areaInfoImg} alt="" />;
-    case 'bedInfo':
-      return <img src={bedInfoImg} alt="" />;
-    case 'maxPeople':
-      return <img src={maxPeopleImg} alt="" />;
-    case 'isProvide':
-      return <img src={isProvideImg} alt="" />;
-  }
+  const myImageSrc = imageMap[type];
+  return <img src={myImageSrc} alt={type} />;
 };
 
 export default RoomInfoBasicImage;
