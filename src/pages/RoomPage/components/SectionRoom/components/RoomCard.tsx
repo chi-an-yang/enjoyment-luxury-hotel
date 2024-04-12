a1daefe7c4cf35ad53570e0645ce93ac6d3946c7
Chian <chian0322@gmail.com>
2024-04-12 21:55:49 +0800

    refactor(roomcard): remove getListObj, use direct return

--- src/pages/RoomPage/components/SectionRoom/components/RoomCard.tsx
+++ src/pages/RoomPage/components/SectionRoom/components/RoomCard.tsx
@@ -4,19 +4,15 @@ import { Rooms } from '@src/apis/home';
 import RoomCardDescription from './RoomCardDescription';
 import { v4 as uuidv4 } from 'uuid';
 
-const getListObj = (url: string) => {
-  return {
-    src: url,
-    name: uuidv4(),
-  };
-};
-
 const RoomCard = ({ card }: { card: Rooms }) => {
   const { breakpoints } = useTheme();
   const isDesktop = useMediaQuery(breakpoints.up('md'));
   const roomList = (card: Rooms) => {
     const { imageUrl, imageUrlList } = card;
-    return [imageUrl, ...imageUrlList].map((url) => getListObj(url));
+    return [imageUrl, ...imageUrlList].map((url) => ({
+      src: url,
+      name: uuidv4(),
+    }));
   };
   return (
     <Stack
