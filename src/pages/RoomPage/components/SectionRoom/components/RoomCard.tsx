6429976c2ff571f36ea126fb721068df23471cf0
Chian <chian0322@gmail.com>
2024-04-12 22:02:29 +0800

    refactor(roomcard): split getRoomList

--- src/pages/RoomPage/components/SectionRoom/components/RoomCard.tsx
+++ src/pages/RoomPage/components/SectionRoom/components/RoomCard.tsx
@@ -4,16 +4,18 @@ import { Rooms } from '@src/apis/home';
 import RoomCardDescription from './RoomCardDescription';
 import { v4 as uuidv4 } from 'uuid';
 
+const getRoomList = (card: Rooms) => {
+  const { imageUrl, imageUrlList } = card;
+  return [imageUrl, ...imageUrlList].map((url) => ({
+    src: url,
+    name: uuidv4(),
+  }));
+};
+
 const RoomCard = ({ card }: { card: Rooms }) => {
   const { breakpoints } = useTheme();
   const isDesktop = useMediaQuery(breakpoints.up('md'));
-  const roomList = (card: Rooms) => {
-    const { imageUrl, imageUrlList } = card;
-    return [imageUrl, ...imageUrlList].map((url) => ({
-      src: url,
-      name: uuidv4(),
-    }));
-  };
+  const roomList = getRoomList(card);
   return (
     <Stack
       width={'100% '}
@@ -25,7 +27,7 @@ const RoomCard = ({ card }: { card: Rooms }) => {
       overflow={'hidden'}
     >
       <Grid display={'flex'} width={'calc(1200px - 524px)'} height={'100%'}>
-        <UiSwiper navigation items={roomList(card)} />
+        <UiSwiper navigation items={roomList} />
       </Grid>
       <RoomCardDescription card={card} />
     </Stack>
